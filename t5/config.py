101a3755f34fe36256400fcf6af0f77e9ff454e6
sunny442k3 <phucvan.ai.442k@gmail.com>
2022-12-30 21:44:59 +0700

    Translate Machine

--- t5/config.py
+++ t5/config.py
@@ -1,8 +1,7 @@
 import os
 import yaml
 from pathlib import Path
-parent = os.path.dirname(os.path.abspath(__file__)) + '/../'
-CONFIG_FILE = parent+os.sep+"config.yml"
+CONFIG_FILE = "./config.yml"
 
 with open(CONFIG_FILE) as f:
     parsed_yaml_file = yaml.load(f, Loader=yaml.FullLoader)
